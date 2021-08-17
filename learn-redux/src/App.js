2521fa3103024d7724067714e6e3fe81d9459312
sagar-aryal <sagar_yal92@outlook.com>
2021-08-17 22:31:15 +0300

    working with reducers and actions

--- learn-redux/src/App.js
+++ learn-redux/src/App.js
@@ -1,9 +1,18 @@
+import React from "react";
 import "./App.css";
 
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./action/action";
+
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <h1>HELLO</h1>
+      <h1>Counter:{counter}</h1>
+      <button onClick={() => dispatch(increment())}>Add</button>
+      <button onClick={() => dispatch(decrement())}>Decrease</button>
     </div>
   );
 }
--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -2,4 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { createStore } from "redux";
+
+import { Provider } from "react-redux";
+import allReducers from "./reducer/reducers";
+
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
