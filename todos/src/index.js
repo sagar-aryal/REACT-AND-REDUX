b888e360691e6e1d4b037bea70aa3e91241d2494
sagar-aryal <sagar_yal92@outlook.com>
2021-08-24 11:47:33 +0300

    initial setup

--- todos/src/index.js
+++ todos/src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import { Provider } from "react-redux";
 import reducers from "./reducers/reducers";
 
-const store = createStore(reducers);
+const store = createStore(
+  reducers,
+  composeWithDevTools()
+  //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
