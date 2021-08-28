43c9e3632c423417ffbd9b2780dbb922fba6f81e
sagar-aryal <sagar_yal92@outlook.com>
2021-08-28 12:55:33 +0300

    creating actions and reducers

---
+++ banking-app/src/state/actions/index.js
@@ -0,0 +1,17 @@
+export const depositMoney = (amount) => {
+  return (dispatch) => {
+    dispatch({
+      type: "DEPOSIT",
+      payload: amount,
+    });
+  };
+};
+
+export const withDrawMoney = (amount) => {
+  return (dispatch) => {
+    dispatch({
+      type: "WITHDRAW",
+      payload: amount,
+    });
+  };
+};
