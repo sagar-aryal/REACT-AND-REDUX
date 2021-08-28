15f510a5c10242bf044ea706b644df1378a33be4
sagar-aryal <sagar_yal92@outlook.com>
2021-08-28 16:34:19 +0300

    functionality is completed

--- banking-app/src/App.js
+++ banking-app/src/App.js
@@ -1,9 +1,24 @@
 import "./App.css";
 
+import { useSelector, useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreaters } from "./state/index";
+
 function App() {
+  const account = useSelector((state) => state.account);
+  const dispatch = useDispatch();
+
+  const { depositMoney, withDrawMoney } = bindActionCreators(
+    actionCreaters,
+    dispatch
+  );
+
   return (
     <div className="App">
       <h1>Banking Application to Withdraw and Deposit Money</h1>
+      <h2>{account}</h2>
+      <button onClick={() => depositMoney(1000)}>Deposit</button>
+      <button onClick={() => withDrawMoney(1000)}>Withdraw</button>
     </div>
   );
 }
