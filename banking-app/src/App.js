6f78e74db2ddb7daa9d14b90a27efff310b71698
sagar-aryal <sagar_yal92@outlook.com>
2021-08-28 11:27:38 +0300

    creating new project with clean code

---
+++ banking-app/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Banking Application to Withdraw and Deposit Money</h1>
+    </div>
+  );
+}
+
+export default App;
