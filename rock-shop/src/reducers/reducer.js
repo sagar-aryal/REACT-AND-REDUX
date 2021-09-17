0c476e6f1d2dbc78746e102775f64f2f37d02e0d
sagar-aryal <sagar_yal92@outlook.com>
2021-09-17 18:14:14 +0300

    view details page fixed

--- rock-shop/src/reducers/reducer.js
+++ rock-shop/src/reducers/reducer.js
@@ -9,12 +9,22 @@ export const productsReducer = (state = [], action) => {
   }
 };
 
-export const selectedProductsReducer = (state = {}, action) => {
+export const selectedProductReducer = (state = {}, action) => {
+  switch (action.type) {
+    case actionTypes.GET_SELECTED_PRODUCT:
+      return action.payload;
+    case actionTypes.REMOVE_SELECTED_PRODUCT:
+      return {};
+
+    default:
+      return state;
+  }
+};
+
+export const addProductReducer = (state = [], action) => {
   switch (action.type) {
     case actionTypes.ADD_PRODUCTS:
-      return { ...state, products: action.payload };
-    case actionTypes.REMOVE_PRODUCTS:
-      return {};
+      return action.payload;
     default:
       return state;
   }
