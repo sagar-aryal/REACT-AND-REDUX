bf164bc4f8c2fee14bc190e07088bc2aebd53d02
sagar-aryal <sagar_yal92@outlook.com>
2021-10-04 13:07:40 +0300

    components added

---
+++ react-context-example/src/components/ComponentB.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ComponentC from "./ComponentC";
+
+const ComponentB = () => {
+  return (
+    <div>
+      <h3>Component B</h3>
+      <ComponentC />
+    </div>
+  );
+};
+
+export default ComponentB;
---
+++ react-context-example/src/components/ComponentC.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ComponentC = () => {
+  return (
+    <div>
+      <h3>Component C</h3>
+    </div>
+  );
+};
+
+export default ComponentC;
